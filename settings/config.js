a8c9e0bc6c8ae7778a890abc9ea4be9508c6be60
Lightning <lightning@lightning-bot.com>
2022-11-17 15:15:08 +0100

    clean config.js

--- settings/config.js
+++ settings/config.js
@@ -3,17 +3,17 @@ import { config } from "dotenv";
 config();
 
 export default {
-  TOKEN: process.env.TOKEN, // bot token
-  MONGO_URI: process.env.MONGO_URI, // for the ticket system to work
-  GUILD_ID: "838151831086825533", // server id where the slashcommands will register
+  TOKEN: process.env.TOKEN,
+  MONGO_URI: process.env.MONGO_URI,
+  GUILD_ID: "838151831086825533",
 
   STATUS: {
-    TEXT: "Tickets", // whatever you want
-    TYPE: "WATCHING", // PLAYING, WATCHING, LISTENING
+    TEXT: "Tickets",
+    TYPE: "WATCHING",
   },
 
   TICKET: {
-    CATEGORY_ID: "1041302484737609808", // where the channel will be created
-    TRANSCRIPT_CHANNEL_ID: "1041302526626123796", // where the html transcript will be sended
+    CATEGORY_ID: "1041302484737609808",
+    TRANSCRIPT_CHANNEL_ID: "1041302526626123796",
   },
 };
