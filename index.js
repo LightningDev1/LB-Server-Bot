4e4259ab33a01500a133a9a241e8b57cf33c90de
Lightning <lightning@lightning-bot.com>
2022-11-17 15:10:30 +0100

    Refactor index.js

--- index.js
+++ index.js
@@ -1,25 +1,57 @@
+import glob from "glob";
 import { Client, Collection } from "discord.js";
+import { promisify } from "util";
 import mongoose from "mongoose";
+
 import config from "./settings/config.js";
-import handler from "./handler/index.js";
+
+const globPromise = promisify(glob);
 
 const client = new Client({ intents: 32767 });
 
 client.commands = new Collection();
 client.slashCommands = new Collection();
 
-mongoose
-  .connect(config.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => console.log("\x1b[32m[+] \x1b[0mDatabase Connected"))
-  .catch((err) =>
-    console.log("\x1b[31m[-] \x1b[0mDatabase Connection Error: " + err)
-  );
+(async () => {
+  try {
+    mongoose.connect(config.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("\x1b[32m[+] \x1b[0mDatabase Connected");
+  } catch (err) {
+    console.log("\x1b[31m[-] \x1b[0mDatabase Connection Error: " + err);
+  }
 
-handler(client);
+  const cwd = process.cwd().replace(/\\/g, "/");
 
-client.login(config.TOKEN);
+  const eventFiles = await globPromise(`${cwd}/events/*.js`);
+  const slashCommandFiles = await globPromise(`${cwd}/commands/*.js`);
+
+  eventFiles.map((filePath) => {
+    filePath = filePath.replace(cwd, ".");
+    import(filePath);
+  });
+
+  const arrayOfSlashCommands = [];
+  slashCommandFiles.map((filePath) => {
+    filePath = filePath.replace(cwd, ".");
+    const file = import(filePath);
+
+    if (!file?.name) return;
+
+    client.slashCommands.set(file.name, file);
+    arrayOfSlashCommands.push(file);
+
+    if (file.userPermissions) file.defaultPermission = false;
+  });
+
+  client.on("ready", async () => {
+    const guild = client.guilds.cache.get(config.GUILD_ID);
+    await guild.commands.set(arrayOfSlashCommands);
+  });
+
+  await client.login(config.TOKEN);
+})();
 
 export default client;
