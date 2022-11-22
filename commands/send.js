bbb3a9dff1690463d9452dbaccadfd3dc91c2ea0
Lightning <lightning@lightning-bot.com>
2022-11-22 14:47:08 +0100

    Finish send.js

--- commands/send.js
+++ commands/send.js
@@ -1,3 +1,6 @@
+import { MessageEmbed } from "discord.js";
+import { config } from "../settings/config.js";
+
 const subCommands = ["rules", "bugreports", "suggestions"];
 
 async function run(client, interaction) {
@@ -12,6 +15,43 @@ async function run(client, interaction) {
       content: "You must be an administrator to use this command",
     });
   }
+
+  const channel = interaction.options.getChannel("channel");
+
+  if (subCommand === "rules") {
+    const embed = new MessageEmbed()
+      .setTitle("LightningBot Rules")
+      .setDescription(
+        "These are the rules for the LightningBot Discord server. You can find our terms of service [here](https://lightning-bot.com/terms). Breaking these rules will result in a punishment."
+      )
+      .setColor("#378cbc");
+
+    for (let i = 0; i < config.RULES.length; i++) {
+      embed.addField(`Rule ${i + 1}`, config.RULES[i]);
+    }
+
+    channel.send({ embeds: [embed] });
+  } else if (subCommand === "bugreports") {
+    const embed = new MessageEmbed()
+      .setTitle("Bug Reports")
+      .setDescription(
+        "If you find a bug in LightningBot, you can report it here. Please make sure to provide as much information as possible."
+      )
+      .setColor("#378cbc");
+
+    channel.send({ embeds: [embed] });
+  } else if (subCommand === "suggestions") {
+    const embed = new MessageEmbed()
+      .setTitle("Suggestions")
+      .setDescription(
+        "Have an idea you would like to see implemented in LightningBot? Post it here! We will do our best to add all suggestions."
+      )
+      .setColor("#378cbc");
+
+    channel.send({ embeds: [embed] });
+  }
+
+  interaction.reply({ content: "Done!", ephemeral: true });
 }
 
 export const command = {
--- settings/config.js
+++ settings/config.js
@@ -20,4 +20,16 @@ export const config = {
     CATEGORY_ID: "1041302484737609808",
     TRANSCRIPT_CHANNEL_ID: "1041302526626123796",
   },
+
+  RULES: [
+    "No Spamming",
+    "No NSFW Content",
+    "No Racism, Homophobia, etc.",
+    "Do not DM staff, open a ticket or send a message in the support channel instead",
+    "Advertising in any way or form is prohibited without permission",
+    "Do not abuse or exploit any bots",
+    "Do not beg for keys, roles, staff, etc.",
+    "Do not send any harmful content (e.g. token loggers, IP loggers, malware)",
+    "Listen to staff and follow their instructions",
+  ],
 };
