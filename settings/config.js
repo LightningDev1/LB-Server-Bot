f01a543c9f150be7faeb88f192450bba0cca36e0
Lightning <lightning@lightning-bot.com>
2022-11-25 11:55:11 +0100

    add release config

--- settings/config.js
+++ settings/config.js
@@ -2,7 +2,9 @@ import { config as dotenvConfig } from "dotenv";
 
 dotenvConfig();
 
-export const config = {
+const DEBUG = process.env.DEBUG === "true";
+
+const sharedConfig = {
   TOKEN: process.env.TOKEN,
   MONGO_URI: process.env.MONGO_URI,
   API_URL: "https://api.lightning-bot.com",
@@ -10,15 +12,6 @@ export const config = {
   STATUS: {
     TEXT: "Tickets",
     TYPE: "WATCHING",
-  },
-
-  GUILD_ID: "838151831086825533",
-  WELCOME_CHANNEL_ID: "1043634985304870992",
-  VERIFIED_USER_ROLE_ID: "768835534440759297",
-
-  TICKET: {
-    CATEGORY_ID: "1041302484737609808",
-    TRANSCRIPT_CHANNEL_ID: "1041302526626123796",
   },
 
   RULES: [
@@ -33,3 +26,31 @@ export const config = {
     "Listen to staff and follow their instructions",
   ],
 };
+
+const debugConfig = {
+  ...sharedConfig,
+
+  GUILD_ID: "838151831086825533",
+  WELCOME_CHANNEL_ID: "1043634985304870992",
+  VERIFIED_USER_ROLE_ID: "768835534440759297",
+
+  TICKET: {
+    CATEGORY_ID: "1041302484737609808",
+    TRANSCRIPT_CHANNEL_ID: "1041302526626123796",
+  },
+};
+
+const releaseConfig = {
+  ...sharedConfig,
+
+  GUILD_ID: "768559129052577842",
+  WELCOME_CHANNEL_ID: "790214961267474452",
+  VERIFIED_USER_ROLE_ID: "768835534440759297",
+
+  TICKET: {
+    CATEGORY_ID: "899395535591583854",
+    TRANSCRIPT_CHANNEL_ID: "1045653703266410496",
+  },
+};
+
+export default DEBUG ? debugConfig : releaseConfig;
