e7878dfc32d007c21a2b63ebbd591d3f80de7ec2
Lightning <lightning@lightning-bot.com>
2023-01-22 11:15:29 +0100

    FAQ

--- commands/send.js
+++ commands/send.js
@@ -19,37 +19,54 @@ async function run(client, interaction) {
 
   const channel = interaction.options.getChannel("channel");
 
-  if (subCommand === "rules") {
-    const embed = new MessageEmbed()
-      .setTitle("LightningBot Rules")
-      .setDescription(
-        "These are the rules for the LightningBot Discord server. You can find our terms of service [here](https://lightning-bot.com/terms). Breaking these rules will result in a punishment."
-      )
-      .setColor("#378cbc");
+  const embed = new MessageEmbed();
 
-    for (let i = 0; i < config.RULES.length; i++) {
-      embed.addField(`Rule ${i + 1}`, config.RULES[i]);
-    }
+  switch (subCommand) {
+    case "rules":
+      embed
+        .setTitle("LightningBot Rules")
+        .setDescription(
+          "These are the rules for the LightningBot Discord server. You can find our terms of service [here](https://lightning-bot.com/terms). Breaking these rules will result in a punishment."
+        )
+        .setColor("#378cbc");
 
-    channel.send({ embeds: [embed] });
-  } else if (subCommand === "bugreports") {
-    const embed = new MessageEmbed()
-      .setTitle("Bug Reports")
-      .setDescription(
-        "If you find a bug in LightningBot, you can report it here. Please make sure to provide as much information as possible."
-      )
-      .setColor("#378cbc");
+      for (let i = 0; i < config.RULES.length; i++) {
+        embed.addField(`Rule ${i + 1}`, config.RULES[i]);
+      }
 
-    channel.send({ embeds: [embed] });
-  } else if (subCommand === "suggestions") {
-    const embed = new MessageEmbed()
-      .setTitle("Suggestions")
-      .setDescription(
-        "Have an idea you would like to see implemented in LightningBot? Post it here! We will do our best to add all suggestions."
-      )
-      .setColor("#378cbc");
+      channel.send({ embeds: [embed] });
+      break;
+    case "bugreports":
+      embed
+        .setTitle("Bug Reports")
+        .setDescription(
+          "If you find a bug in LightningBot, you can report it here. Please make sure to provide as much information as possible."
+        )
+        .setColor("#378cbc");
 
-    channel.send({ embeds: [embed] });
+      channel.send({ embeds: [embed] });
+      break;
+    case "suggestions":
+      embed
+        .setTitle("Suggestions")
+        .setDescription(
+          "Have an idea you would like to see implemented in LightningBot? Post it here! We will do our best to add all suggestions."
+        )
+        .setColor("#378cbc");
+
+      channel.send({ embeds: [embed] });
+      break;
+    case "faq":
+      embed
+        .setTitle("Frequently Asked Questions")
+        .setDescription(
+          "Have a question about LightningBot? Check out if it's already answered in our FAQ at [lightning-bot.com/faq](https://lightning-bot.com/faq) first!" +
+            " If not, create a ticket or ask it in the support channel. We will try to answer as soon as possible."
+        )
+        .setColor("#378cbc");
+
+      channel.send({ embeds: [embed] });
+      break;
   }
 
   interaction.reply({ content: "Done!", ephemeral: true });
@@ -93,6 +110,20 @@ export const command = {
     {
       name: "suggestions",
       description: "Send suggestions message",
+      type: "SUB_COMMAND",
+      options: [
+        {
+          name: "channel",
+          description: "Channel where the message will be sent",
+          type: "CHANNEL",
+          required: true,
+          channelTypes: ["GUILD_TEXT"],
+        },
+      ],
+    },
+    {
+      name: "faq",
+      description: "Send FAQ message",
       type: "SUB_COMMAND",
       options: [
         {
